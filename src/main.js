46085f633d1befdc77e0f6357595b0365baeec36
WarmthDawn <warmth06@sina.com>
2021-07-06 09:15:00 +0800

    :arrow_up: 添加vue-router依赖

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import installElementPlus from './plugins/element'
+import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(router)
 installElementPlus(app)
 app.use(store).mount('#app')
