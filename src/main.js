5ef7ac5021179a21734bd61faca200f4b1ddfd7e
smq <1179731085@qq.com>
2021-07-07 16:32:46 +0800

    :construction: 服务器详细信息2
    服务器设置卡片

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import './plugins/utils'
+
 import '@/assets/icon-file/iconfont'
 import '@/assets/icon-file/iconfont.css'
 import 'animate.css'
--- src/views/server/Info.vue
+++ src/views/server/Info.vue
@@ -1,13 +1,27 @@
 <template>
-<h1>info</h1>
+  <el-row :gutter="20">
+    <el-col :span="8" :xs="24" :sm="12" :lg="8" :xl="6">
+      <settings-inspect :id="id"></settings-inspect>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import SettingsInspect from '@/components/server/info/SettingsInspect.vue'
+
 export default {
-    name: 'Info'
+    name: 'Info',
+    components: {
+        SettingsInspect
+    },
+    props: ['id'],
 }
 </script>
 
 <style scoped>
+
+.el-col {
+  padding: 10px;
+}
 
 </style>
