4b74592b3bdd90c7f3b0317bee70779a89041750
WarmthDawn <warmth06@sina.com>
2021-07-17 03:26:05 +0800

    :ambulance: 修复部署环境nginx websocket转发问题

--- src/api/command.ts
+++ src/api/command.ts
@@ -10,7 +10,7 @@ export async function attachServer(id: number): Promise<WebSocket> {
         uri = 'ws:'
     }
     uri += '//' + loc.host
-    uri = `ws://localhost:7777/command/attach/${id}`
+    uri += `socket/command/attach/${id}`
     const ws = new WebSocket(uri)
 
     return await new Promise((resolve, reject) => {
