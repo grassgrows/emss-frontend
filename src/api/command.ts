3d4c51b0d8ce23c8ca0598ab4f20d1ac504f6ddb
WarmthDawn <warmth06@sina.com>
2021-07-23 03:21:41 +0800

    :ambulance: 修复ws连接没有认证的bug

--- src/api/command.ts
+++ src/api/command.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import result from '@/utils/result'
+import store from '@/store'
 
 export async function attachServer(id: number): Promise<WebSocket> {
     const loc = window.location
@@ -11,6 +12,7 @@ export async function attachServer(id: number): Promise<WebSocket> {
     }
     uri += '//' + loc.host
     uri += `/socket/command/attach/${id}`
+    uri += `?token=${store.state.authToken}`
     const ws = new WebSocket(uri)
 
     return await new Promise((resolve, reject) => {
